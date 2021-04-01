2c1f6add7db9c8e17a5156ec3bf83bf4bf14afbc
Ken Van Haren <kenvanharen@gmail.com>
2021-03-31 20:30:35 -0700

    Csv -> records

--- dcp/data_copy/copiers/to_memory/file_to_memory.py
+++ dcp/data_copy/copiers/to_memory/file_to_memory.py
@@ -1,112 +1,87 @@
-from __future__ import annotations
-
-from pyarrow import json as pa_json
-from snapflow.core.typing.inference import conform_records_to_schema
-from snapflow.schema.base import Schema
-from snapflow.storage.data_copy.base import (
-    Conversion,
-    DiskToBufferCost,
+from dcp.data_format.formats.file_system.json_lines_file import JsonLinesFileFormat
+import json
+from dcp.utils.common import DcpJsonEncoder
+from dcp.utils.data import read_csv, write_csv
+from dcp.data_format.formats.file_system.csv_file import CsvFileFormat
+from dcp.storage.file_system.engines.local import FileSystemStorageApi
+from dcp.utils.pandas import dataframe_to_records
+from dcp.storage.base import FileSystemStorageClass, MemoryStorageClass, StorageApi
+from dcp.data_copy.costs import (
     DiskToMemoryCost,
     FormatConversionCost,
-    NetworkToBufferCost,
-    NetworkToMemoryCost,
-    NoOpCost,
-    datacopy,
+    MemoryToMemoryCost,
 )
-from snapflow.storage.data_formats import (
-    DatabaseCursorFormat,
-    DatabaseTableFormat,
-    DatabaseTableRef,
-    DatabaseTableRefFormat,
-    RecordsFormat,
-)
-from snapflow.storage.data_formats.arrow_table import ArrowTableFormat
-from snapflow.storage.data_formats.delimited_file import DelimitedFileFormat
-from snapflow.storage.data_formats.delimited_file_object import (
-    DelimitedFileObjectFormat,
-    DelimitedFileObjectIteratorFormat,
-)
-from snapflow.storage.data_formats.json_lines_file import JsonLinesFileFormat
-from snapflow.storage.data_records import as_records
-from snapflow.storage.db.api import DatabaseStorageApi
-from snapflow.storage.db.utils import result_proxy_to_records
-from snapflow.storage.file_system import FileSystemStorageApi
-from snapflow.storage.storage import (
-    DatabaseStorageClass,
-    FileSystemStorageClass,
-    PythonStorageApi,
-    MemoryStorageClass,
-    StorageApi,
-)
-from snapflow.utils.data import read_csv
+from dcp.data_format.formats.memory.dataframe import DataFrameFormat
+from dcp.storage.memory.memory_records_object import as_records
+from dcp.data_format.formats.memory.records import Records, RecordsFormat
+from dcp.storage.memory.engines.python import PythonStorageApi
+from dcp.data_copy.base import CopyRequest, datacopy
+
+from pyarrow import json as pa_json
 
 
 @datacopy(
     from_storage_classes=[FileSystemStorageClass],
-    from_data_formats=[DelimitedFileFormat],
+    from_data_formats=[CsvFileFormat],
     to_storage_classes=[MemoryStorageClass],
     to_data_formats=[RecordsFormat],
     cost=DiskToMemoryCost + FormatConversionCost,
 )
-def copy_delim_file_to_records(
-    from_name: str,
-    to_name: str,
-    conversion: Conversion,
-    from_storage_api: StorageApi,
-    to_storage_api: StorageApi,
-    schema: Schema,
-):
-    assert isinstance(from_storage_api, FileSystemStorageApi)
-    assert isinstance(to_storage_api, PythonStorageApi)
-    with from_storage_api.open(from_name) as f:
+def copy_csv_file_to_records(req: CopyRequest):
+    assert isinstance(req.from_storage_api, FileSystemStorageApi)
+    assert isinstance(req.to_storage_api, PythonStorageApi)
+    with req.from_storage_api.open(req.from_name) as f:
         records = list(read_csv(f.readlines()))
-        mdr = as_records(records, data_format=RecordsFormat, schema=schema)
-        mdr = mdr.conform_to_schema()
-        to_storage_api.put(to_name, mdr)
+        mdr = as_records(records, data_format=RecordsFormat, schema=req.schema)
+        req.to_storage_api.put(req.to_name, mdr)
+        # This cast step is necessary because CSVs preserve no logical type information
+        req.to_format_handler.cast_to_schema(
+            req.to_name, req.to_storage_api.storage, req.schema
+        )
 
 
-@datacopy(
-    from_storage_classes=[FileSystemStorageClass],
-    from_data_formats=[DelimitedFileFormat],
-    to_storage_classes=[MemoryStorageClass],
-    to_data_formats=[DelimitedFileObjectFormat],
-    cost=DiskToBufferCost,
-)
-def copy_delim_file_to_file_object(
-    from_name: str,
-    to_name: str,
-    conversion: Conversion,
-    from_storage_api: StorageApi,
-    to_storage_api: StorageApi,
-    schema: Schema,
-):
-    assert isinstance(from_storage_api, FileSystemStorageApi)
-    assert isinstance(to_storage_api, PythonStorageApi)
-    with from_storage_api.open(from_name) as f:
-        mdr = as_records(f, data_format=DelimitedFileObjectFormat, schema=schema)
-        mdr = mdr.conform_to_schema()
-        to_storage_api.put(to_name, mdr)
+# @datacopy(
+#     from_storage_classes=[FileSystemStorageClass],
+#     from_data_formats=[DelimitedFileFormat],
+#     to_storage_classes=[MemoryStorageClass],
+#     to_data_formats=[DelimitedFileObjectFormat],
+#     cost=DiskToBufferCost,
+# )
+# def copy_delim_file_to_file_object(
+#     from_name: str,
+#     to_name: str,
+#     conversion: Conversion,
+#     from_storage_api: StorageApi,
+#     to_storage_api: StorageApi,
+#     schema: Schema,
+# ):
+#     assert isinstance(from_storage_api, FileSystemStorageApi)
+#     assert isinstance(to_storage_api, PythonStorageApi)
+#     with from_storage_api.open(from_name) as f:
+#         mdr = as_records(f, data_format=DelimitedFileObjectFormat, schema=schema)
+#         mdr = mdr.conform_to_schema()
+#         to_storage_api.put(to_name, mdr)
 
 
-@datacopy(
-    from_storage_classes=[FileSystemStorageClass],
-    from_data_formats=[JsonLinesFileFormat],
-    to_storage_classes=[MemoryStorageClass],
-    to_data_formats=[ArrowTableFormat],
-    cost=DiskToMemoryCost,  # TODO: conversion cost might be minimal cuz in C?
-)
-def copy_json_file_to_arrow(
-    from_name: str,
-    to_name: str,
-    conversion: Conversion,
-    from_storage_api: StorageApi,
-    to_storage_api: StorageApi,
-    schema: Schema,
-):
-    assert isinstance(from_storage_api, FileSystemStorageApi)
-    assert isinstance(to_storage_api, PythonStorageApi)
-    pth = from_storage_api.get_path(from_name)
-    at = pa_json.read_json(pth)
-    mdr = as_records(at, data_format=ArrowTableFormat, schema=schema)
-    mdr = mdr.conform_to_schema()
-    to_storage_api.put(to_name, mdr)
+# @datacopy(
+#     from_storage_classes=[FileSystemStorageClass],
+#     from_data_formats=[JsonLinesFileFormat],
+#     to_storage_classes=[MemoryStorageClass],
+#     to_data_formats=[ArrowTableFormat],
+#     cost=DiskToMemoryCost,  # TODO: conversion cost might be minimal cuz in C?
+# )
+# def copy_json_file_to_arrow(
+#     from_name: str,
+#     to_name: str,
+#     conversion: Conversion,
+#     from_storage_api: StorageApi,
+#     to_storage_api: StorageApi,
+#     schema: Schema,
+# ):
+#     assert isinstance(from_storage_api, FileSystemStorageApi)
+#     assert isinstance(to_storage_api, PythonStorageApi)
+#     pth = from_storage_api.get_path(from_name)
+#     at = pa_json.read_json(pth)
+#     mdr = as_records(at, data_format=ArrowTableFormat, schema=schema)
+#     mdr = mdr.conform_to_schema()
+#     to_storage_api.put(to_name, mdr)
--- tests/test_data_copy/test_file_to_memory.py
+++ tests/test_data_copy/test_file_to_memory.py
@@ -1,53 +1,53 @@
 from __future__ import annotations
-
+from dcp.data_copy.copiers.to_memory.file_to_memory import copy_csv_file_to_records
+from dcp.data_format.handler import get_handler, get_handler_for_name
+from dcp.utils.data import read_csv
+from dcp.data_format.formats.file_system.csv_file import CsvFileFormat
+from dcp.data_copy.copiers.to_file.memory_to_file import copy_records_to_csv_file
+from dcp.data_format.formats.memory.records import RecordsFormat
+from dcp.storage.file_system.engines.local import FileSystemStorageApi
 import tempfile
+from dcp.data_format.formats.database.base import DatabaseTableFormat
+from dcp.data_copy.copiers.to_database.memory_to_database import copy_records_to_db
+import warnings
+from dcp.storage.database.api import DatabaseApi, DatabaseStorageApi
+from dcp.storage.base import DatabaseStorageClass, LocalPythonStorageEngine, Storage
+from dcp.data_copy.base import Conversion, CopyRequest, StorageFormat
+from dcp.storage.memory.memory_records_object import as_records
+from dcp.storage.memory.engines.python import PythonStorageApi, new_local_python_storage
 
-import pyarrow as pa
-from snapflow.storage.data_copy.base import Conversion, StorageFormat
-from snapflow.storage.data_copy.file_to_memory import (
-    copy_delim_file_to_records,
-    copy_json_file_to_arrow,
+from typing import Type
+
+import pytest
+from tests.utils import (
+    test_records_schema,
+    conformed_test_records,
 )
-from snapflow.storage.data_formats import DelimitedFileFormat, RecordsFormat
-from snapflow.storage.data_formats.arrow_table import ArrowTableFormat
-from snapflow.storage.data_formats.json_lines_file import JsonLinesFileFormat
-from snapflow.storage.file_system import FileSystemStorageApi
-from snapflow.storage.storage import (
-    LocalPythonStorageEngine,
-    PythonStorageApi,
-    Storage,
-    new_local_python_storage,
-)
-from tests.utils import TestSchema4
 
 
 def test_file_to_mem():
     dr = tempfile.gettempdir()
     s: Storage = Storage.from_url(f"file://{dr}")
     fs_api: FileSystemStorageApi = s.get_api()
-    mem_api: PythonStorageApi = new_local_python_storage().get_api()
+    mem_s = new_local_python_storage()
+    mem_api: PythonStorageApi = mem_s.get_api()
     name = "_test"
     fs_api.write_lines_to_file(name, ["f1,f2", "hi,2"])
     # Records
     records_obj = [{"f1": "hi", "f2": 2}]
-    conversion = Conversion(
-        StorageFormat(s.storage_engine, DelimitedFileFormat),
-        StorageFormat(LocalPythonStorageEngine, RecordsFormat),
-    )
-    copy_delim_file_to_records.copy(
-        name, name, conversion, fs_api, mem_api, schema=TestSchema4
-    )
+    req = CopyRequest(name, s, name, RecordsFormat, mem_s, test_records_schema)
+    copy_csv_file_to_records.copy(req)
     assert mem_api.get(name).records_object == records_obj
 
-    # Json lines
-    name = "_json_test"
-    fs_api.write_lines_to_file(name, ['{"f1":"hi","f2":2}'])
-    conversion = Conversion(
-        StorageFormat(s.storage_engine, JsonLinesFileFormat),
-        StorageFormat(LocalPythonStorageEngine, ArrowTableFormat),
-    )
-    copy_json_file_to_arrow.copy(
-        name, name, conversion, fs_api, mem_api, schema=TestSchema4
-    )
-    expected = pa.Table.from_pydict({"f1": ["hi"], "f2": [2]})
-    assert mem_api.get(name).records_object == expected
+    # # Json lines
+    # name = "_json_test"
+    # fs_api.write_lines_to_file(name, ['{"f1":"hi","f2":2}'])
+    # conversion = Conversion(
+    #     StorageFormat(s.storage_engine, JsonLinesFileFormat),
+    #     StorageFormat(LocalPythonStorageEngine, ArrowTableFormat),
+    # )
+    # copy_json_file_to_arrow.copy(
+    #     name, name, conversion, fs_api, mem_api, schema=TestSchema4
+    # )
+    # expected = pa.Table.from_pydict({"f1": ["hi"], "f2": [2]})
+    # assert mem_api.get(name).records_object == expected
