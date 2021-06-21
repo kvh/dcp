3e38958320080d9bdcf9b2ddc259b815984203a8
Ken Van Haren <kenvanharen@gmail.com>
2021-06-21 16:47:19 -0500

    Drop...cascade

--- dcp/storage/database/api.py
+++ dcp/storage/database/api.py
@@ -42,9 +42,7 @@ def dispose_all(keyword: Optional[str] = None):
 
 class DatabaseApi:
     def __init__(
-        self,
-        url: str,
-        json_serializer: Callable = None,
+        self, url: str, json_serializer: Callable = None,
     ):
         self.url = url
         self.json_serializer = (
@@ -64,9 +62,7 @@ class DatabaseApi:
         if key in _sa_engines:
             return _sa_engines[key]
         self.eng = sqlalchemy.create_engine(
-            self.url,
-            json_serializer=self.json_serializer,
-            echo=False,
+            self.url, json_serializer=self.json_serializer, echo=False,
         )
         _sa_engines[key] = self.eng
         return self.eng
@@ -141,7 +137,7 @@ class DatabaseApi:
         self.execute_sql(f"create table {to_name} as select * from {name}")
 
     def remove(self, name: str):
-        self.execute_sql(f"drop table {name}")
+        self.execute_sql(f"drop table {name} cascade")
 
     def rename_table(self, table_name: str, new_name: str):
         self.execute_sql(f"alter table {table_name} rename to {new_name}")
@@ -165,9 +161,7 @@ class DatabaseApi:
         self.execute_sql(insert_sql)
 
     def create_table_from_sql(
-        self,
-        name: str,
-        sql: str,
+        self, name: str, sql: str,
     ):
         sql = self.clean_sub_sql(sql)
         create_sql = f"""
@@ -235,8 +229,7 @@ class DatabaseApi:
 
 class DatabaseStorageApi(DatabaseApi, StorageApi):
     def __init__(
-        self,
-        storage: storage,
+        self, storage: storage,
     ):
         super().__init__(storage.url)
         self.storage = storage
