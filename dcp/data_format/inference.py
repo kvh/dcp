8d9628c85a65442e4e4b2f39394878a96ed4d5c6
Ken Van Haren <kenvanharen@gmail.com>
2022-03-13 16:10:09 -0700

    Upgrade common-model

--- dcp/data_format/inference.py
+++ dcp/data_format/inference.py
@@ -1,17 +1,15 @@
 from typing import Union
 from commonmodel.base import Schema
-from dcp.utils.common import rand_str
+from dcp.utils.common import rand_str, utcnow
 
-GENERATED_SCHEMA_NAMESPACE = "_generated"
+GENERATED_SCHEMA_PREFIX = "_GeneratedSchema_"
 
 
 def generate_auto_schema(fields, **kwargs) -> Schema:
-    auto_name = f"GeneratedSchema{rand_str(10)}"
+    auto_name = f"{GENERATED_SCHEMA_PREFIX}{rand_str(10)}"
     args = dict(
         name=auto_name,
-        namespace=GENERATED_SCHEMA_NAMESPACE,
-        version="0.0.1",
-        description="Automatically inferred schema",
+        description=f"Automatically inferred from data at {utcnow()}",
         unique_on=[],
         implementations=[],
         fields=fields,
@@ -22,9 +20,8 @@ def generate_auto_schema(fields, **kwargs) -> Schema:
 
 def is_generated_schema(schema: Union[str, Schema]) -> bool:
     if isinstance(schema, Schema):
-        schema = schema.key
-    return schema.startswith(GENERATED_SCHEMA_NAMESPACE)
+        schema = schema.name
+    return schema.startswith(GENERATED_SCHEMA_PREFIX)
 
 
 is_auto_schema = is_generated_schema  # Deprecated name
-
